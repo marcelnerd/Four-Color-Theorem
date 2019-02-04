11adbf9d431f801a3b8ecad58cbec75fb1aa75e4
marcelnerd <31940874+marcelnerd@users.noreply.github.com>
2019-02-04 11:44:09 -0600

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		//TODO Allow user to enter the polygons to be used; This could be done using terminal or gui
+		//TODO Feed polygons into algorithm and solve the map; This could be displayed in the gui as it is happening, or not
+		//TODO Display final result in gui
+	}
+
+}
